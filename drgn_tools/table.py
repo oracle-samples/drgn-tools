b94470677b7cf32b2a6a1e22e70f28a76a7ae62a
Pradyumn Rahar <pradyumn.rahar@oracle.com>
2023-10-19 08:35:39 +0530

    Add print_dictionary helper
    Signed-off-by: Pradyumn Rahar <pradyumn.rahar@oracle.com>

--- drgn_tools/table.py
+++ drgn_tools/table.py
@@ -2,6 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 import sys
 from typing import Any
+from typing import Dict
 from typing import List
 from typing import Optional
 
@@ -37,6 +38,40 @@ def print_table(
                 str(val).ljust(col_width + 2)
                 for (val, col_width) in zip(entry, col_widths)
             ),
+            file=out,
+        )
+
+    if outfile:
+        out.close()
+
+
+def print_dictionary(
+    dictionary: Dict[str, Any],
+    outfile: Optional[str] = None,
+    report: bool = False,
+) -> None:
+    """
+    Align and print the data
+
+    :param dictionary: dictionary to print
+    :param outfile: A file to write the output to.
+    :param report: Open the file in append mode.
+    :returns: None
+    """
+    lcol_length = 10
+    for title in dictionary:
+        lcol_length = max(len(title), lcol_length)
+
+    out = sys.stdout
+    if outfile and report:
+        out = open(outfile, "a")
+        print("\n", file=out)
+    elif outfile:
+        out = open(outfile, "w")
+
+    for title in dictionary:
+        print(
+            f"{title.ljust(lcol_length)}: {dictionary[title]}",
             file=out,
         )
 
