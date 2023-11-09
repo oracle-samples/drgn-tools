2518537f199163c23df02099bf74784e7d2a5cc7
Stephen Brennan <stephen.s.brennan@oracle.com>
2023-11-09 09:33:08 -0800

    dentry: replace dentry_path with any_mount() version
    For the corelens report, we want to see a full filepath from the root,
    not just the path within a single filesystem. But we don't have any
    information about the mount that was used to reach a dentry, since it
    came straight from the hash table. Introduce dentry_path_any_mount()
    which is like dentry_path(), except that it simply uses the first mount
    point of each superblock until it reaches the root. This matches the
    behavior of oled dentrycache, but it's also quite useful.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- drgn_tools/dentry.py
+++ drgn_tools/dentry.py
@@ -12,12 +12,73 @@ from typing import Optional
 import drgn
 from drgn import Object
 from drgn import Program
-from drgn.helpers.linux.fs import dentry_path
+from drgn.helpers.linux.list import list_for_each_entry
 
 from drgn_tools.corelens import CorelensModule
 from drgn_tools.itertools import count
 from drgn_tools.itertools import take
 from drgn_tools.table import print_table
+
+
+MNT_INTERNAL = 0x4000
+
+
+def sb_first_mount_point(sb: Object) -> Optional[Object]:
+    """
+    Return the first mountpoint of the superblock
+
+    A single filesystem instance can be mounted at several locations, so the
+    super_block has a list of instances. When iterating over the dentry cache,
+    we want the full path and don't care too much about _which_ path we get. We
+    just want to have a valid filesystem path. So return any arbitrary mount
+    point, the first one in the list. If the list is empty (unlikely except
+    during an unmount race) or if we are at the root filesystem, return None.
+
+    :param sb: ``struct super_block *``
+    :returns: ``struct dentry *`` or None
+    """
+    for mount in list_for_each_entry(
+        "struct mount", sb.s_mounts.address_of_(), "mnt_instance"
+    ):
+        mnt_parent = mount.mnt_parent.read_()
+        if mount.mnt.mnt_flags & MNT_INTERNAL:
+            continue
+        if mnt_parent == mount:
+            return None
+        return mount.mnt_mountpoint.read_()
+    return None
+
+
+def dentry_path_any_mount(dentry: Object) -> bytes:
+    """
+    Like dentry_path(), but don't require a path/mount. Just pick one
+    arbitrarily
+
+    :param dentry: ``struct dentry *``
+    """
+    dentry = dentry.read_()
+    d_op = dentry.d_op.read_()
+    if d_op and d_op.d_dname:
+        return b"[" + dentry.d_inode.i_sb.s_type.name.string_() + b"]"
+
+    components = []
+    while True:
+        # reading dentry_val allows us to get all the fields of dentry at once
+        dentry_val = dentry[0].read_()
+        if dentry_val.d_parent == dentry:
+            dentry = sb_first_mount_point(dentry_val.d_sb)
+            if not dentry:
+                break
+            else:
+                continue
+        d_parent = dentry_val.d_parent
+        components.append(dentry_val.d_name.name.string_())
+        components.append(b"/")
+        dentry = d_parent
+    if components:
+        return b"".join(reversed(components))
+    else:
+        return b"/"
 
 
 def for_each_dentry_in_hashtable(prog: Program) -> Iterator[Object]:
@@ -175,7 +236,7 @@ def print_dentry_table(
                 hex(d.d_inode.value_()),
                 int(d_count(d)),
                 file_type,
-                dentry_path(d).decode(),
+                dentry_path_any_mount(d).decode(),
             ]
         else:
             dentry_stats = [
@@ -183,9 +244,9 @@ def print_dentry_table(
                 hex(d.d_sb.value_()),
                 hex(d.d_inode.value_()),
                 file_type,
-                dentry_path(d).decode(),
+                dentry_path_any_mount(d).decode(),
             ]
-        dentry_table.append(dentry_stats)
+        dentry_table.append(dentry_stats)  # type: ignore
     print_table(dentry_table)
 
 
