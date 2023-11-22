oracle-samples/drgn-tools

commit 7e791a9a33d8c9e527007f039d620ea428fae38b

block: Reduce the chance of inconsistency for inflight I/O
When reading I/O requests out of a live kernel, there's a pretty good
chance that the I/O could complete and the data structure could get
freed during the time between when we found the data structure, and when
we actually print it.

This means that the list iterators, and the printing functions, all risk
reading stale and potentially overwritten data.

We can help reduce the chances of problems with two approaches:

1. Read the requests into a list ahead of time. This will help avoid the
list iterators getting stale data.

2. Once we have all the requests, dereference the pointer and use
.read_() to get a copy of the data that won't change even as the kernel
memory changes.

With these two changes, the chance of reading false data is reduced (but
not eliminated). It should reduce some flakiness in tests.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

drgn_tools/block.py
@@ -196,7 +196,7 @@ def rq_pending_time_ms(rq: Object) -> int:
     """
     Get io pending time in ms
 
-    :param rq: ``struct request *``
+    :param rq: ``struct request *`` or ``struct request``
     :returns: request pending time
     """
     prog = rq.prog_
@@ -261,7 +261,7 @@ def rq_op(rq: Object) -> str:
     """
     Get request operation name
 
-    :param rq: ``struct request *``
+    :param rq: ``struct request *`` or ``struct request``
     :returns: combined request operation enum name as str
     """
     prog = rq.prog_
@@ -277,7 +277,7 @@ def rq_flags(rq: Object) -> str:
     """
     Get request operation flags
 
-    :param rq: ``struct request *``
+    :param rq: ``struct request *`` or ``struct request``
     :returns: operation flags of the request
     """
     # uek4 didn't have this member
@@ -385,7 +385,15 @@ def dump_inflight_io(prog: drgn.Program, diskname: str = "all") -> None:
         name = disk.disk_name.string_().decode()
         if diskname != "all" and diskname != name:
             continue
-        for hwq, rq in for_each_mq_pending_request(disk.queue):
+        # Read the requests all at once into a list, and use read_() to
+        # transform them into "values" - this is in case we are running on a
+        # live system, as it reduces the chances of in-memory changes breaking
+        # things.
+        mq_pending = [
+            (hwq[0].read_(), rq.value_(), rq[0].read_())
+            for hwq, rq in for_each_mq_pending_request(disk.queue)
+        ]
+        for hwq, rq_ptr, rq in mq_pending:
             # for mq disk from same hba host who are sharing hwq.tags
             # check gendisk to dump io only from this particular disk.
             if (hwq.flags & BLK_MQ_F_TAG_SHARED) != 0 and request_target(
@@ -397,7 +405,7 @@ def dump_inflight_io(prog: drgn.Program, diskname: str = "all") -> None:
                 % (
                     name,
                     hwq.value_(),
-                    rq.value_(),
+                    rq_ptr,
                     rq_op(rq),
                     rq_flags(rq),
                     rq.__sector,
@@ -405,13 +413,17 @@ def dump_inflight_io(prog: drgn.Program, diskname: str = "all") -> None:
                     rq_pending_time_ms(rq),
                 )
             )
-        for rq in for_each_sq_pending_request(disk.queue):
+        sq_pending = [
+            (rq.value_(), rq[0].read_())
+            for rq in for_each_sq_pending_request(disk.queue)
+        ]
+        for rq_ptr, rq in sq_pending:
             print(
                 "%-20s %-20s %-20lx %-16s\n%-20s %-20d %-20d %-16d"
                 % (
                     name,
                     "-",
-                    rq.value_(),
+                    rq_ptr,
                     rq_op(rq),
                     rq_flags(rq),
                     rq.__sector,

commit 1307e5c6633d24465d65b3401f647f16fa1b46ca

block: save hwq address before coverting it to value obj
Fixes: 7e791a9a33d8("block: Reduce the chance of inconsistency for inflight I/O")
Signed-off-by: Junxiao Bi <junxiao.bi@oracle.com>

drgn_tools/block.py
@@ -390,10 +390,10 @@ def dump_inflight_io(prog: drgn.Program, diskname: str = "all") -> None:
         # live system, as it reduces the chances of in-memory changes breaking
         # things.
         mq_pending = [
-            (hwq[0].read_(), rq.value_(), rq[0].read_())
+            (hwq.value_(), hwq[0].read_(), rq.value_(), rq[0].read_())
             for hwq, rq in for_each_mq_pending_request(disk.queue)
         ]
-        for hwq, rq_ptr, rq in mq_pending:
+        for hwq_ptr, hwq, rq_ptr, rq in mq_pending:
             # for mq disk from same hba host who are sharing hwq.tags
             # check gendisk to dump io only from this particular disk.
             if (hwq.flags & BLK_MQ_F_TAG_SHARED) != 0 and request_target(
@@ -404,7 +404,7 @@ def dump_inflight_io(prog: drgn.Program, diskname: str = "all") -> None:
                 "%-20s %-20lx %-20lx %-16s\n%-20s %-20d %-20d %-16d"
                 % (
                     name,
-                    hwq.value_(),
+                    hwq_ptr,
                     rq_ptr,
                     rq_op(rq),
                     rq_flags(rq),