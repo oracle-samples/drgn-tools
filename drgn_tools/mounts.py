oracle-samples/drgn-tools

commit 4db1ec84482886c5f4709fb3413cd8daf7d90bd5

Added mounts module
This module lists all the mount points from a given vmcore

Signed-off-by: Gautham Ananthakrishna <gautham.ananthakrishna@oracle.com>

drgn_tools/mounts.py
@@ -1,0 +1,58 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+import argparse
+from typing import List
+
+import drgn.helpers.linux.fs
+
+from drgn_tools.corelens import CorelensModule
+from drgn_tools.table import print_table
+
+
+def get_mountinfo(prog: drgn.Program) -> List[List[str]]:
+    """
+    Get all the mount points from the vmcore
+
+    :param prog: drgn program
+    :returns: List of mount points
+    """
+
+    mount_table = list()
+
+    mounts = prog["init_task"].nsproxy.mnt_ns
+    for mnt in drgn.helpers.linux.fs.for_each_mount(mounts):
+        devname = mnt.mnt_devname
+        fstype = mnt.mnt.mnt_sb.s_type.name
+        mntpt = drgn.helpers.linux.fs.d_path(
+            mnt.mnt.address_of_(), mnt.mnt_mountpoint
+        )
+
+        mount_stats = [
+            devname.string_().decode("utf-8"),
+            fstype.string_().decode("utf-8"),
+            mntpt.decode("utf-8"),
+        ]
+        mount_table.append(mount_stats)
+    return mount_table
+
+
+def mountinfo(prog: drgn.Program) -> None:
+    """
+    Print all the mount points from the vmcore
+
+    :param prog: drgn program
+    :returns: None
+    """
+    mnt_tbl = get_mountinfo(prog)
+    mnt_tbl.insert(0, ["-------", "------", "-------"])
+    mnt_tbl.insert(0, ["DEVNAME", "TYPE", "DIRNAME"])
+    print_table(mnt_tbl)
+
+
+class Mounts(CorelensModule):
+    """Print info about all mount points"""
+
+    name = "mounts"
+
+    def run(self, prog: drgn.Program, args: argparse.Namespace) -> None:
+        mountinfo(prog)

tests/test_mounts.py
@@ -1,0 +1,30 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+from operator import itemgetter
+
+import pytest
+
+from drgn_tools import mounts
+
+
+def test_print_mounts(prog):
+    mounts.mountinfo(prog)
+
+
+def get_proc_mounts():
+    fields_0_2_1 = itemgetter(0, 2, 1)
+    proc_mount_table = list()
+    with open("/proc/mounts", "r") as f:
+        for line in f.readlines():
+            field_0, field_2, field_1 = fields_0_2_1(line.split())
+            proc_mount_table.append([field_0, field_2, field_1])
+    return proc_mount_table
+
+
+@pytest.mark.skip_vmcore("*")
+def test_show_mounts(prog):
+    prog_table = mounts.get_mountinfo(prog)
+    proc_table = get_proc_mounts()
+
+    for row in proc_table:
+        assert row in prog_table

commit b6a3b63c260898df7568c49a6573706ccf07a54a

mount: fix filesystem path in output
The mnt_mountpoint dentry is actually a dentry in the parent filesystem.
So, using mnt.mnt as the vfsmount for d_path(), we need to use the
mnt.mnt_parent.mnt. This fixes #35, the broken test:

_______________________________ test_show_mounts _______________________________

prog = <_drgn.Program object at 0x55daadeaf4c8>

    @pytest.mark.skip_vmcore("*")
    def test_show_mounts(prog):
        prog_table = mounts.get_mountinfo(prog)
        proc_table = get_proc_mounts()

        for row in proc_table:
>           assert row in prog_table
E           AssertionError: assert ['securityfs', 'securityfs', '/sys/kernel/security'] in [['none', 'rootfs', '/'], ['sysfs', 'sysfs', '/sys'], ['proc', 'proc', '/proc'], ['devtmpfs', 'devtmpfs', '/dev'], ['securityfs', 'securityfs', '/kernel/security'], ['tmpfs', 'tmpfs', '/shm'], ...]

Fixes: 4db1ec8 ("Added mounts module")

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

drgn_tools/mounts.py
@@ -24,7 +24,7 @@ def get_mountinfo(prog: drgn.Program) -> List[List[str]]:
         devname = mnt.mnt_devname
         fstype = mnt.mnt.mnt_sb.s_type.name
         mntpt = drgn.helpers.linux.fs.d_path(
-            mnt.mnt.address_of_(), mnt.mnt_mountpoint
+            mnt.mnt_parent.mnt.address_of_(), mnt.mnt_mountpoint
         )
 
         mount_stats = [