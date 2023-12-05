bfa25fb7d9a9f78a3bff4889d6611124382171a0
Stephen Brennan <stephen.s.brennan@oracle.com>
2023-12-05 15:52:00 -0800

    Automatic documentation update

--- _static/conf.py
+++ _static/conf.py
@@ -45,7 +45,7 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-nitpick_ignore = [('py:class', 'drgn_tools.itertools.T')]
+nitpick_ignore = [("py:class", "drgn_tools.itertools.T")]
 
 # -- Options for HTML output -------------------------------------------------
 
