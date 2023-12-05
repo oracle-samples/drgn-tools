5bc37da29f97bc069c6fd88d2422c4c5c14409b8
Stephen Brennan <stephen.s.brennan@oracle.com>
2023-12-05 14:59:07 -0800

    ps: run "ps -m" by default in bulk corelens runs
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- drgn_tools/task.py
+++ drgn_tools/task.py
@@ -210,6 +210,8 @@ class Taskinfo(CorelensModule):
 
     name = "ps"
 
+    default_args = ["-m"]
+
     def add_args(self, parser: argparse.ArgumentParser) -> None:
         parser.add_argument(
             "-m",
