40f8448572de5b92272b1fa6692768e04f82e03d
Anil Lal Palakunnathu Kunnengeri <anil.palakunnathu.kunnengeri@oracle.com>
2023-12-11 14:05:46 -0500

    Display runq details per cpu
    Signed-off-by: Anil Lal Palakunnathu Kunnengeri <anil.palakunnathu.kunnengeri@oracle.com>

---
+++ tests/test_runq.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+from drgn import ProgramFlags
+
+import drgn_tools.runq as runq
+
+
+def test_run_queue(prog):
+    if ProgramFlags.IS_LIVE & prog.flags:
+        return
+    runq.run_queue(prog)
