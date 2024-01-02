2ce9b7a099d920fb5692b1e0f3706a61cd6c51e9
Junxiao Bi <junxiao.bi@oracle.com>
2024-01-02 22:11:19 +0000

    dm: add helper to dump device mapper devices
    The output is similar with 'dmshow' from crash-pykdump
    
    Signed-off-by: Junxiao Bi <junxiao.bi@oracle.com>

---
+++ tests/test_dm.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+from drgn_tools import dm
+
+
+def test_show_dm(prog):
+    dm.show_dm(prog)
