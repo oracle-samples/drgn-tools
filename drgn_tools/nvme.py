686a6d8f08fbb705bf13c863fbf520f56f4b5df3
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-01-04 15:09:29 -0800

    nvme: updates to allow it to run on my laptop
    So many changes to these code paths...
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- drgn_tools/nvme.py
+++ drgn_tools/nvme.py
@@ -1,43 +1,24 @@
 # Copyright (c) 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 import argparse
+from typing import Any
 from typing import Iterable
+from typing import List
 
+from drgn import cast
 from drgn import container_of
 from drgn import Object
 from drgn import Program
 from drgn.helpers.common.type import enum_type_to_class
 from drgn.helpers.linux import list_for_each_entry
 from drgn.helpers.linux.block import for_each_disk
+from drgn.helpers.linux.xarray import xa_for_each
 
+from drgn_tools.block import for_each_hw_queue
 from drgn_tools.corelens import CorelensModule
-from drgn_tools.module import KernelModule
-from drgn_tools.module import load_module_debuginfo
+from drgn_tools.module import ensure_debuginfo
+from drgn_tools.table import print_table
 from drgn_tools.util import enum_name_get
-
-
-def load_modules(prog: Program) -> bool:
-    """
-    Load nvme modules
-
-    :param prog: drgn program
-    :returns: True if module loaded, False if not.
-    """
-    mods = []
-    mod = KernelModule.find(prog, "nvme")
-    if not mod:
-        print("No nvme module detected.")
-        return False
-    mods.append(mod)
-
-    mod = KernelModule.find(prog, "nvme-core")
-    if not mod:
-        print("No nvme-core module detected.")
-        return False
-    mods.append(mod)
-
-    load_module_debuginfo(prog, mods, extract=True)
-    return True
 
 
 def for_each_nvme_disk(prog: Program) -> Iterable[Object]:
@@ -47,102 +28,97 @@ def for_each_nvme_disk(prog: Program) -> Iterable[Object]:
     :param prog: Program currently debugging
     :returns: iterable of ``struct nvme_ns *`` in the program
     """
-    if not load_modules(prog):
-        return None
     for disk in for_each_disk(prog):
-        if disk.disk_name.string_().decode().startswith("nvme"):
-            yield Object(
-                prog, "struct nvme_ns *", value=disk.private_data.value_()
-            )
+        if disk.disk_name.string_().startswith(b"nvme"):
+            yield cast("struct nvme_ns *", disk.private_data)
 
 
 def show_ns_info(prog: Program) -> None:
     """
     Display namepsace information for each NVMe device
     """
-    if not load_modules(prog):
+    msg = ensure_debuginfo(prog, ["nvme", "nvme_core"])
+    if msg:
+        print(msg)
         return
-    fmtHeader = "{:12}{:20}{:10}{:10}{:15}\n"
-    fmtValues = "{:12}{:<20x}{:^10}{:^10}{:^15}\n"
-    print(
-        fmtHeader.format(
-            "NAME", "NVME_NS", "REFCOUNT", "PI_TYPE", "NVME_NS_FLAG"
-        )
-    )
+    rows = [["NAME", "NVME_NS", "REFCOUNT", "PI_TYPE", "NVME_NS_FLAG"]]
     for nvme_ns in for_each_nvme_disk(prog):
         if hasattr(nvme_ns.kref.refcount, "refs"):
             refcount = nvme_ns.kref.refcount.refs.counter.value_()
         else:
             refcount = nvme_ns.kref.refcount.counter.value_()
 
-        print(
-            fmtValues.format(
+        rows.append(
+            [
                 nvme_ns.disk.disk_name.string_().decode(),
-                nvme_ns.value_(),
+                "{:x}".format(nvme_ns.value_()),
                 refcount,
                 nvme_ns.pi_type.value_(),
                 nvme_ns.flags.value_(),
-            )
+            ]
         )
+    print_table(rows)
 
 
 def show_ctrl_info(prog: Program) -> None:
     """
     Display information about each NVMe controller
     """
-    if not load_modules(prog):
+    msg = ensure_debuginfo(prog, ["nvme", "nvme_core"])
+    if msg:
+        print(msg)
         return
-    fmtHeader = "{:12}{:20}{:15}\n"
-    fmtValues = "{:12}{:<20x}{:15}\n"
     NvmeCtrlState = enum_type_to_class(
         prog.type("enum nvme_ctrl_state"), "NvmeCtrlState", prefix="NVME_CTRL_"
     )
-    print(
-        fmtHeader.format("NAME", "NVME_CTRL", "NVME_CTRL_STATE", "INSTANCE_ID")
-    )
+    rows: List[List[Any]] = [
+        ["NAME", "NVME_CTRL", "NVME_CTRL_STATE", "INSTANCE_ID"]
+    ]
     for nvme_ns in for_each_nvme_disk(prog):
         nvme_ns_ctrl_state = nvme_ns.ctrl.state.value_()
         ctrl_state = enum_name_get(
             NvmeCtrlState, nvme_ns_ctrl_state, "UNKNOWN"
         )
-        print(
-            fmtValues.format(
+        rows.append(
+            [
                 nvme_ns.disk.disk_name.string_().decode(),
-                nvme_ns.ctrl.address_of_().value_(),
+                "{:x}".format(nvme_ns.ctrl.address_of_().value_()),
                 ctrl_state,
                 nvme_ns.ctrl.instance.value_(),
-            )
+            ]
         )
+    print_table(rows)
 
 
 def show_firmware_info(prog: Program) -> None:
     """
     Display NVMe firmware information
     """
-    if not load_modules(prog):
+    msg = ensure_debuginfo(prog, ["nvme", "nvme_core"])
+    if msg:
+        print(msg)
         return
-    fmtHeader = "{:12}{:20}{:24}{:20}\n"
-    fmtValues = "{:12}{:20}{:24}{:20}\n"
-    print(fmtHeader.format("NAME", "SERIAL#", "MODEL", "FIRMWARE VERSION"))
+    rows = [["NAME", "SERIAL#", "MODEL", "FIRMWARE VERSION"]]
     for nvme_ns in for_each_nvme_disk(prog):
         if hasattr(nvme_ns, "head"):
-            print(
-                fmtValues.format(
+            rows.append(
+                [
                     nvme_ns.disk.disk_name.string_().decode(),
-                    nvme_ns.head.subsys.serial.string_().decode().rstrip(),
-                    nvme_ns.head.subsys.model.string_().decode().rstrip(),
+                    nvme_ns.head.subsys.serial.string_().decode().strip(),
+                    nvme_ns.head.subsys.model.string_().decode().strip(),
                     nvme_ns.head.subsys.firmware_rev.string_().decode(),
-                )
+                ]
             )
         else:
-            print(
-                fmtValues.format(
+            rows.append(
+                [
                     nvme_ns.disk.disk_name.string_().decode(),
                     "UNSUPPORTED",
                     "UNSUPPORTED",
                     "UNSUPPORTED",
-                )
+                ]
             )
+    print_table(rows)
 
 
 def show_queue_info(prog: Program) -> None:
@@ -151,12 +127,12 @@ def show_queue_info(prog: Program) -> None:
     Value of -1 indicates parameter does not exist in that version
 
     """
-    if not load_modules(prog):
+    msg = ensure_debuginfo(prog, ["nvme", "nvme_core"])
+    if msg:
+        print(msg)
         return
-    fmtHeader = "{:12}{:13}{:10}{:7}{:10}{:10}{:10}\n"
-    fmtValues = "{:12}{:^13}{:^10}{:^7}{:^10d}{:^10}{:^10}\n"
-    print(
-        fmtHeader.format(
+    rows = [
+        [
             "NAME",
             "#allocatedQ",
             "#OnlineQ",
@@ -164,8 +140,8 @@ def show_queue_info(prog: Program) -> None:
             "#DefaultQ",
             "#ReadQ",
             "#PollQ",
-        )
-    )
+        ]
+    ]
     for nvme_ns in for_each_nvme_disk(prog):
         nvme_dev = container_of(nvme_ns.ctrl, "struct nvme_dev", "ctrl")
         if hasattr(nvme_dev, "nr_allocated_queues"):
@@ -182,8 +158,8 @@ def show_queue_info(prog: Program) -> None:
             nr_ioqueue_read = -1
             nr_ioqueue_poll = -1
 
-        print(
-            fmtValues.format(
+        rows.append(
+            [
                 nvme_ns.disk.disk_name.string_().decode(),
                 num_q,
                 nvme_dev.online_queues.value_(),
@@ -191,102 +167,124 @@ def show_queue_info(prog: Program) -> None:
                 nr_ioqueue_default,
                 nr_ioqueue_read,
                 nr_ioqueue_poll,
-            )
+            ]
         )
+    print_table(rows)
 
 
 def show_queue_map(prog: Program) -> None:
     """
     Display mapping of each NVMe queue, its HW & SW context to the CPU
     """
-    if not load_modules(prog):
+    msg = ensure_debuginfo(prog, ["nvme", "nvme_core"])
+    if msg:
+        print(msg)
         return
-    fmtHeader = "{:^10}{:^20}{:^20}{:^20}{:^10}"
-    fmtValues = "{:^10}{:20x}{:20x}{:20x}{:^10}"
     for nvme_ns in for_each_nvme_disk(prog):
-        displayDev = (
+        print(
             "Queue info for device "
             + nvme_ns.disk.disk_name.string_().decode()
         )
-        print(displayDev)
-        print(fmtHeader.format("Queue#", "nvmeQ", "hw_ctx", "ctx", "CPU#"))
-        for nr in range(nvme_ns.queue.nr_hw_queues):
-            hw_ctx = Object(
-                prog,
-                "struct blk_mq_hw_ctx",
-                address=nvme_ns.queue.queue_hw_ctx[nr],
-            )
-            nvmeq = Object(
-                prog, "struct nvme_queue", address=hw_ctx.driver_data.value_()
-            )
+        rows = [["Queue#", "nvmeQ", "hw_ctx", "ctx", "CPU#"]]
+        for hw_ctx in for_each_hw_queue(nvme_ns.queue):
+            nvmeq = cast("struct nvme_queue *", hw_ctx.driver_data)
             for i in range(hw_ctx.nr_ctx):
-                ctx = Object(prog, "struct blk_mq_ctx", address=hw_ctx.ctxs[i])
-                print(
-                    fmtValues.format(
+                ctx = hw_ctx.ctxs[i]
+                rows.append(
+                    [
                         hw_ctx.queue_num.value_(),
-                        nvmeq.address_of_().value_(),
-                        hw_ctx.address_of_().value_(),
-                        ctx.address_of_().value_(),
+                        "{:x}".format(nvmeq.value_()),
+                        "{:x}".format(hw_ctx.value_()),
+                        "{:x}".format(ctx.value_()),
                         ctx.cpu.value_(),
-                    )
+                    ]
                 )
+        print_table(rows)
+
+
+def for_each_msi_desc(pdev: Object) -> Iterable[Object]:
+    list = None
+    if hasattr(pdev, "msi_list"):
+        # Older kernels have msi_list in struct pci_dev.
+        list = pdev.msi_list
+    elif hasattr(pdev.dev, "msi_list"):
+        # Starting with 4a7cc8316705 ("genirq/MSI: Move msi_list from struct
+        # pci_dev to struct device"), it was moved to struct device
+        list = pdev.dev.msi_list
+    elif hasattr(pdev.dev.msi.data, "list"):
+        # Starting with 125282cd4f33 ("genirq/msi: Move descriptor list to
+        # struct msi_device_data"), it was moved again, into the msi_device_data
+        # structure.
+        list = pdev.dev.msi.data.list
+
+    if list is not None:
+        return list_for_each_entry(
+            "struct msi_desc", list.address_of_(), "list"
+        )
+
+    msidata = pdev.dev.msi.data
+    prog = msidata.prog_
+
+    if hasattr(msidata, "__store"):
+        # And then, the list got turned into an xarray! Wow. cd6cf06590b9
+        # ("genirq/msi: Convert storage to xarray")
+        xarray = msidata.__store
+    else:
+        # But wait, there's more. Then, the xarry got split into multiple
+        # arrays, one for each MSI domain.
+        xarray = msidata.__domains[prog.constant("MSI_DEFAULT_DOMAIN")].store
+    return (
+        cast("struct msi_desc *", obj)
+        for _, obj in xa_for_each(xarray.address_of_())
+    )
 
 
 def show_msi_mask(prog: Program) -> None:
     """
     Display if the MSI has been masked for each NVMe queue
     """
-    if not load_modules(prog):
+    msg = ensure_debuginfo(prog, ["nvme", "nvme_core"])
+    if msg:
+        print(msg)
         return
-    fmtHeader = "{:^10}{:^10}{:^0}{:^10}"
-    fmtValues = "{:^10}{:^10}{:^10}{:^10x}"
+
+    rows = [["Queue#", "IRQ#", "cq_vector", "MSI mask"]]
     for nvme_ns in for_each_nvme_disk(prog):
         displayDev = (
             "MSI info for device " + nvme_ns.disk.disk_name.string_().decode()
         )
         print(displayDev)
-        print(fmtHeader.format("Queue#", "IRQ#", "cq_vector", "MSI mask"))
-        for nr in range(nvme_ns.queue.nr_hw_queues):
-            hw_ctx = Object(
-                prog,
-                "struct blk_mq_hw_ctx",
-                address=nvme_ns.queue.queue_hw_ctx[nr],
-            )
-            nvmeq = Object(
-                prog, "struct nvme_queue", address=hw_ctx.driver_data.value_()
-            )
+        rows = [["Queue#", "IRQ#", "cq_vector", "MSI mask"]]
+        for hw_ctx in for_each_hw_queue(nvme_ns.queue):
+            nvmeq = cast("struct nvme_queue *", hw_ctx.driver_data)
             pdev = container_of(nvmeq.dev.dev, "struct pci_dev", "dev")
 
-            if hasattr(pdev.dev, "msi_list"):
-                msi_holder = pdev.dev
-            else:
-                msi_holder = pdev
-
-            for msi_desc in list_for_each_entry(
-                "struct msi_desc", msi_holder.msi_list.address_of_(), "list"
-            ):
-                if (
-                    nvmeq.cq_vector.value_()
-                    == msi_desc.msi_attrib.entry_nr.value_()
-                ):
-                    if hasattr(msi_desc, "masked"):
-                        print(
-                            fmtValues.format(
-                                nvmeq.qid.value_(),
-                                msi_desc.irq.value_(),
-                                msi_desc.msi_attrib.entry_nr.value_(),
-                                msi_desc.masked.value_(),
-                            )
-                        )
+            for msi_desc in for_each_msi_desc(pdev):
+                if hasattr(msi_desc, "pci"):
+                    if hasattr(msi_desc.pci.msi_attrib, "entry_nr"):
+                        entry_nr = msi_desc.pci.msi_attrib.entry_nr
                     else:
-                        print(
-                            fmtValues.format(
-                                nvmeq.qid.value_(),
-                                msi_desc.irq.value_(),
-                                msi_desc.msi_attrib.entry_nr.value_(),
-                                msi_desc.msix_ctrl.value_(),
-                            )
-                        )
+                        entry_nr = msi_desc.msi_index
+                else:
+                    entry_nr = msi_desc.msi_attrib.entry_nr
+
+                if nvmeq.cq_vector.value_() == entry_nr.value_():
+                    row = [
+                        nvmeq.qid.value_(),
+                        msi_desc.irq.value_(),
+                        entry_nr.value_(),
+                    ]
+
+                    mask = 0
+                    if hasattr(msi_desc, "masked"):
+                        mask = msi_desc.masked.value_()
+                    elif hasattr(msi_desc, "msix_ctrl"):
+                        mask = msi_desc.msix_ctrl.value_()
+                    else:
+                        mask = msi_desc.pci.msix_ctrl.value_()
+                    row.append("{:x}".format(mask))
+                    rows.append(row)
+        print_table(rows)
 
 
 class NvmeModule(CorelensModule):
@@ -294,6 +292,8 @@ class NvmeModule(CorelensModule):
 
     name = "nvme"
     skip_unless_have_kmod = "nvme"
+
+    debuginfo_kmods = ["nvme", "nvme_core"]
 
     default_args = [
         "--firmware",
@@ -335,13 +335,19 @@ class NvmeModule(CorelensModule):
     def run(self, prog: Program, args: argparse.Namespace) -> None:
         if args.firmware:
             show_firmware_info(prog)
+            print()
         if args.ctrl:
             show_ctrl_info(prog)
+            print()
         if args.queue:
             show_queue_info(prog)
+            print()
         if args.namespace:
             show_ns_info(prog)
+            print()
         if args.queuemap:
             show_queue_map(prog)
+            print()
         if args.msimask:
             show_msi_mask(prog)
+            print()
