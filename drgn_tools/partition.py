d8da9ec34aae4af1b6321f9806d18ce036500c46
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-01-24 13:13:30 -0800

    partition: Some nitpicks and improvements
    There were a couple nits I had with partitioninfo - the docstrings had
    the wrong backticks, and then there was the partition info return type.
    Representing it as a list seemed really silly compared to a named tuple.
    Once that was done, it was easier to use the new Table() API to print
    the table, and that allowed me to right-align the column.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- drgn_tools/partition.py
+++ drgn_tools/partition.py
@@ -4,6 +4,7 @@
 Helper to print partition information
 """
 import argparse
+from typing import NamedTuple
 
 from drgn import Object
 from drgn import Program
@@ -14,71 +15,94 @@ from drgn.helpers.linux.device import MAJOR
 from drgn.helpers.linux.device import MINOR
 
 from drgn_tools.corelens import CorelensModule
-from drgn_tools.table import print_table
+from drgn_tools.table import Table
 
 
-def get_partinfo_from_blkdev_struct(part: Object) -> list:
+class PartInfo(NamedTuple):
     """
-    Collect partition information from `struct block_device`
+    Partition info, from either ``struct block_device`` or ``struct hd_struct``
+    """
+
+    major: int
+    minor: int
+    name: str
+    start_sect: int
+    nr_sects: int
+    ro: bool
+    obj: Object
+
+
+def get_partinfo_from_blkdev_struct(part: Object) -> PartInfo:
+    """
+    Collect partition information from ``struct block_device``
     Returns a list with partition information for the given partition.
     """
     devt = part.bd_dev.value_()
     name = part_name(part).decode()
     start_sect = int(part.bd_start_sect)
     nr_sects = int(part.bd_inode.i_size.value_() / 512)
-    ro = "Yes" if part.bd_read_only else "No"
-    return [
+    return PartInfo(
         MAJOR(devt),
         MINOR(devt),
         name,
         start_sect,
         nr_sects,
-        ro,
-        hex(part.value_()),
-    ]
+        bool(part.bd_read_only),
+        part,
+    )
 
 
-def get_partinfo_from_hd_struct(part: Object) -> list:
+def get_partinfo_from_hd_struct(part: Object) -> PartInfo:
     """
-    Collects partition information from `struct hd_struct`
+    Collects partition information from ``struct hd_struct``
     Returns a list with partition information for the given partition.
     """
     devt = part_devt(part)
     name = part_name(part).decode()
     start_sect = int(part.start_sect)
     nr_sects = part.nr_sects.value_()
-    ro = "Yes" if int(part.policy) else "No"
-    return [
+    return PartInfo(
         MAJOR(devt),
         MINOR(devt),
         name,
         start_sect,
         nr_sects,
-        ro,
-        hex(part.value_()),
-    ]
+        bool(part.policy),
+        part,
+    )
 
 
 def print_partition_info(prog: Program) -> None:
     """
     Prints partition information
     """
-    output = [["MAJOR", "MINOR", "NAME", "START", "SECTORS", "READ-ONLY"]]
+    table = Table(
+        [
+            "MAJOR",
+            "MINOR",
+            "NAME",
+            "START:>",
+            "SECTORS:>",
+            "READ-ONLY",
+            "OBJECT:016x",  # will be replaced, see below
+        ]
+    )
     part_is_blkdev = -1
     for part in for_each_partition(prog):
         if part_is_blkdev == -1:
             if "block_device" in part.type_.type_name():
                 part_is_blkdev = 1
-                output[0].append("BLOCK DEVICE")
+                table.header[-1] = "BLOCK DEVICE"
             else:
                 part_is_blkdev = 0
-                output[0].append("HD STRUCT")
+                table.header[-1] = "HD STRUCT"
         if part_is_blkdev == 1:
-            output.append(get_partinfo_from_blkdev_struct(part))
+            info = get_partinfo_from_blkdev_struct(part)
         else:
-            output.append(get_partinfo_from_hd_struct(part))
+            info = get_partinfo_from_hd_struct(part)
+        table.row(*info._replace(obj=info.obj.value_()))
 
-    print_table(output)
+    table.write()
 
 
 class PartitionInfo(CorelensModule):
