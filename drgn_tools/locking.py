c6db735dd0459ec0a871dc940705b6729cc1663c
Imran Khan <imran.f.khan@oracle.com>
2024-02-02 21:28:16 +1100

    locking: mutex: sanitize mutex_owner and add a helper to check mutex state.
    Signed-off-by: Imran Khan <imran.f.khan@oracle.com>

--- drgn_tools/locking.py
+++ drgn_tools/locking.py
@@ -6,6 +6,8 @@ Helper for linux kernel locking
 from typing import Iterable
 
 import drgn
+from drgn import cast
+from drgn import NULL
 from drgn import Object
 from drgn import Program
 from drgn.helpers.linux.list import list_for_each_entry
@@ -20,12 +22,53 @@ MUTEX_FLAGS = 0x7
 
 
 def mutex_owner(prog: Program, mutex: drgn.Object) -> drgn.Object:
-    owner = mutex.owner.counter.value_()
-    if owner < 0:
-        owner = 2**64 + owner - 1
-    Owner = owner & (~MUTEX_FLAGS)
-    struct_owner = Object(prog, "struct task_struct *", value=Owner)
-    return struct_owner
+    """
+    Get mutex owner
+
+    :param prog: drgn program
+    :param mutex: ``struct mutex *``
+    :param : ``struct task_struct *``
+    :returns: ``struct task_struct *`` corresponding to owner,
+              or NULL if there is no owner
+    """
+
+    try:
+        owner = mutex.owner
+        if owner.type_.type_name() == "struct task_struct *":
+            return owner
+        elif owner.value_():
+            # Since Linux kernel commit 3ca0ff571b09 ("locking/mutex: Rework mutex::owner")
+            # (in v4.10) count has been replaced with atomic_long_t owner that contains the
+            # owner information (earlier available under task_struct *owner) and uses lower
+            # bits for mutex state
+            owner = cast("unsigned long", owner.counter.read_()) & ~MUTEX_FLAGS
+            return Object(
+                prog, "struct task_struct", address=owner
+            ).address_of_()
+        else:
+            return NULL(prog, "struct task_struct *")
+    except AttributeError:
+        print("Mutex does not have owner information")
+        return NULL(prog, "struct task_struct *")
+
+
+def mutex_is_locked(lock: Object) -> bool:
+    """
+    Check if a mutex is locked or not
+
+    :param lock: ``struct mutex *``
+    :returns: True if mutex is locked, False otherwise.
+    """
+
+    try:
+        count = lock.count
+        if count.counter.value_() != 1:
+            return True
+        else:
+            return False
+    except AttributeError:
+        ret = True if mutex_owner(lock.prog_, lock) else False
+        return ret
 
 
 def show_lock_waiter(
