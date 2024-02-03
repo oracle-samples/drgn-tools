f7a9db9285af03ec6ee86c24a4206543377342a0
Imran Khan <imran.f.khan@oracle.com>
2024-02-03 19:39:19 +1100

    locking: osq: add helpers for MCS locks.
    Signed-off-by: Imran Khan <imran.f.khan@oracle.com>

--- drgn_tools/locking.py
+++ drgn_tools/locking.py
@@ -11,12 +11,112 @@ from drgn import NULL
 from drgn import Object
 from drgn import Program
 from drgn.helpers.linux.list import list_for_each_entry
+from drgn.helpers.linux.percpu import per_cpu
 from drgn.helpers.linux.sched import task_cpu
 from drgn.helpers.linux.sched import task_state_to_char
 
 from drgn_tools.bt import bt
 from drgn_tools.task import task_lastrun2now
+from drgn_tools.util import per_cpu_owner
 from drgn_tools.util import timestamp_str
+
+######################################
+# osq lock
+######################################
+_OSQ_UNLOCKED_VAL = 0
+
+
+def osq_is_locked(osq: Object) -> bool:
+    """
+    Check if an osq is locked or not
+
+    :param osq: ``struct optimistic_spin_queue *``
+    :returns: True if osq is locked, False otherwise.
+    """
+
+    return osq.tail.counter.value_() != _OSQ_UNLOCKED_VAL
+
+
+def get_osq_owner_cpu(osq: Object) -> int:
+    """
+    Get owner cpu of an osq.
+
+    :param osq: ``struct optimistic_spin_queue *``
+    :returns: cpu that owns this osq, -1 otherwise
+    """
+
+    if not osq_is_locked(osq):
+        return -1
+
+    prog = osq.prog_
+    tail = osq.tail.counter.value_()
+    osq_node = per_cpu(prog["osq_node"], tail - 1)
+    if not osq_node.prev.value_():
+        return tail - 1
+
+    while osq_node.prev and osq_node.prev.next == osq_node.address_of_():
+        osq_node = Object(
+            prog, "struct optimistic_spin_node", address=osq_node.prev.value_()
+        )
+
+    return per_cpu_owner("osq_node", osq_node)
+
+
+def tail_osq_node_to_spinners(osq_node: Object) -> Iterable[int]:
+    """
+    Given an osq_node, find owner and all spinners of same osq
+
+    MCS/OSQ locks are unique in the sense that for these locks both
+    the owners and waiters spin, albeit on different things.
+    The owner spins, usually to optimistically grab a sleeping lock but
+    the waiters spin on some per-cpu entity.
+
+    :param osq_node: ``struct optimistic_spin_node *``
+    :returns: Iterator of spinning CPUs
+    """
+
+    prog = osq_node.prog_
+    tail_osq_node = osq_node
+    while (
+        tail_osq_node.prev
+        and tail_osq_node.prev.next == tail_osq_node.address_of_()
+    ):
+        yield per_cpu_owner("osq_node", tail_osq_node)
+        tail_osq_node = Object(
+            prog,
+            "struct optimistic_spin_node",
+            address=tail_osq_node.prev.value_(),
+        )
+
+    yield per_cpu_owner("osq_node", tail_osq_node)
+
+
+def for_osq_owner_and_each_spinner(osq: Object) -> Iterable[int]:
+    """
+    Given an osq, find its owner and all spinners
+
+    MCS/OSQ locks are unique in the sense that for these locks both
+    the owners and waiters spin, albeit on different things.
+    The owner spins, usually to optimistically grab a sleeping lock but
+    the waiters spin on some per-cpu entity.
+
+    :param osq: ``struct optimistic_spin_queue *``
+    :returns: Iterator of spinning CPUs
+    """
+    if not osq_is_locked(osq):
+        return -1
+
+    prog = osq.prog_
+    tail = osq.tail.counter.value_()
+    tail_cpu = tail - 1
+    tail_osq_node = per_cpu(prog["osq_node"], tail_cpu)
+
+    if not tail_osq_node.prev.value_():
+        return -1
+
+    for cpu in tail_osq_node_to_spinners(tail_osq_node):
+        yield cpu
+
 
 MUTEX_FLAGS = 0x7
 
