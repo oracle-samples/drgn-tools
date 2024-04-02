c03d3d2dc7eb87f8e8a2642c272d026a0ab0f4ca
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-04-02 16:44:14 -0700

    cli: add automatic CTF usage
    The drgn-tools CLI can automatically find local DWARF info, or fetch it
    if not available. However, it has no nice automation for CTF. Update the
    CLI to accept "-D" to force DWARF, "-C" to force CTF, or without either,
    to use them in the following priority:
    
    1. DWARF already available on the system
    2. CTF already available on the system
    3. DWARF fetched from wherever it is available
    
    This is similar to the corelens logic, but it's not the same. Perhaps we
    can look into a breaking change that unifies the CLI arguments, but for
    now this works well.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- drgn_tools/cli.py
+++ drgn_tools/cli.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Oracle and/or its affiliates.
+# Copyright (c) 2024, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 """
 The drgn_tools CLI entry point.
@@ -10,8 +10,11 @@ import argparse
 import importlib
 import os
 import sys
+from pathlib import Path
 from typing import Any
 from typing import Dict
+from typing import Optional
+from typing import Tuple
 
 from drgn import Program
 
@@ -30,6 +33,14 @@ from drgn.cli import run_interactive
 from drgn.cli import version_header
 
 
+try:
+    from drgn.helpers.linux.ctf import load_ctf  # noqa
+
+    HAVE_CTF = True
+except (ImportError, ModuleNotFoundError):
+    HAVE_CTF = False
+
+
 CLI_HELPERS = {
     "drgn_tools.bt": ["bt"],
     "drgn_tools.printk": ["dmesg"],
@@ -43,6 +54,73 @@ CLI_HELPERS = {
 }
 
 
+def _get_ctf_path(release: str, args: argparse.Namespace) -> Optional[str]:
+    if args.ctf_file and os.path.isfile(args.ctf_file):
+        return args.ctf_file
+    default = f"/lib/modules/{release}/kernel/vmlinux.ctfa"
+    if os.path.isfile(default):
+        return default
+    by_vmcore = Path(args.vmcore).parent / "vmlinux.ctfa"
+    if by_vmcore.is_file():
+        return str(by_vmcore)
+    return None
+
+
+def _set_debuginfo(
+    prog: Program, release: str, args: argparse.Namespace
+) -> Tuple[str, str]:
+    problems = [f"Kernel version: {release}"]
+
+    # First try to find DWARF on the system (unless we're forcing CTF). If
+    # found, continue to loading modules: we're committed to DWARF. If not, try
+    # CTF before fetching DWARF debuginfo.
+    if not args.ctf:
+        vmlinux = find_debuginfo(prog, "vmlinux")
+        if vmlinux:
+            prog.load_debug_info([vmlinux])
+            load_module_debuginfo(
+                prog, extract=args.extract_modules, quiet=True
+            )
+            return "DWARF", str(vmlinux)
+        else:
+            problems.append("DWARF debuginfo not found for vmlinux")
+
+    # Try CTF so long as we're not forcing DWARF.
+    if not args.dwarf:
+        ctf_path = _get_ctf_path(release, args)
+        if ctf_path and HAVE_CTF:
+            load_ctf(prog, ctf_path)
+            return "CTF", ctf_path
+        elif ctf_path:
+            problems.append(
+                "CTF found, but drgn is not built with CTF support"
+            )
+        elif not HAVE_CTF:
+            problems.append(
+                "CTF debuginfo is not found, and drgn has no CTF support"
+            )
+        else:
+            problems.append("CTF debuginfo was not found")
+
+    # Now try to fetch DWARF via a fetcher.
+    if not args.ctf:
+        print("Fetching debuginfo...")
+        fetched = fetch_debuginfo(release, ["vmlinux"])
+        if fetched and "vmlinux" in fetched:
+            vmlinux = fetched["vmlinux"]
+            prog.load_debug_info([vmlinux])
+            load_module_debuginfo(
+                prog, extract=args.extract_modules, quiet=True
+            )
+            return "DWARF", str(vmlinux)
+        else:
+            problems.append("DWARF debuginfo could not be fetched")
+
+    # Nothing worked, sorry!
+    problem_str = "\n".join(problems)
+    sys.exit(f"error: failed to find debuginfo:\n{problem_str}")
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="drgn wrapper")
     parser.add_argument("vmcore", help="vmcore to open")
@@ -52,18 +130,35 @@ def main() -> None:
         action="store_true",
         help=(
             "extract the debuginfo for all loaded modules (requires "
-            "a vmlinux repository)"
+            "a vmlinux repository, implies --dwarf)"
         ),
     )
     parser.add_argument(
         "--ctf",
         "-C",
-        help=(
-            "the path of a vmlinux.ctfa file to use instead of searching for"
-            " DWARF DEBUGINFO"
-        ),
+        action="store_true",
+        help="force the use of CTF",
+    )
+    parser.add_argument(
+        "--ctf-file",
+        help="specify a CTF file to use (implies --ctf)",
+    )
+    parser.add_argument(
+        "--dwarf",
+        "-D",
+        action="store_true",
+        help="force the use of DWARF debuginfo",
     )
     args = parser.parse_args()
+
+    # Set the implied arguments
+    if args.ctf_file:
+        args.ctf = True
+    if args.extract_modules:
+        args.dwarf = True
+
+    if args.ctf and args.dwarf:
+        sys.exit("error: --dwarf and --ctf conflict, use only one")
 
     prog = Program()
     try:
@@ -78,50 +173,25 @@ def main() -> None:
                 sys.exit("error: no permission to open /proc/kcore")
 
     release = prog["UTS_RELEASE"].string_().decode("ascii")
-    print(f"Detected version: {release}")
-
-    more = ""
-    if not args.ctf:
-        vmlinux = find_debuginfo(prog, "vmlinux")
-        if not vmlinux:
-            print("vmlinux not found, trying to fetch")
-            fetched = fetch_debuginfo(release, ["vmlinux"])
-            if not fetched:
-                print("error: could not find vmlinux")
-                sys.exit(1)
-            vmlinux = fetched["vmlinux"]
-        if not vmlinux:
-            print("error: vmlinux not found, and no failed to fetch it")
-            sys.exit(1)
-
-        print(f"Using {str(vmlinux)}")
-        more += "\n"
-        prog.load_debug_info([vmlinux])
-
-        load_module_debuginfo(prog, extract=args.extract_modules, quiet=True)
-
-    else:
-        try:
-            from drgn.helpers.linux.ctf import load_ctf
-
-            load_ctf(prog, args.ctf)
-            print(f"Using CTF {os.path.abspath(args.ctf)}")
-        except ImportError:
-            sys.exit("error: drgn is not built with CTF")
+    db_kind, db_file = _set_debuginfo(prog, release, args)
 
     def banner_func(banner: str) -> str:
         header = version_header()
+        ctf_blurb = ", with CTF)" if HAVE_CTF else ", without CTF)"
+        header = header[:-1] + ctf_blurb
         imports = "\n"
         for mod_name, names in CLI_HELPERS.items():
             imports += f">>> from {mod_name} import {', '.join(names)}\n"
+        db_info = f"Using {db_kind}: {db_file}"
+        if db_kind == "DWARF":
+            db_info += "\n" + str(get_module_load_summary(prog))
         return (
             header
             + "\n"
             + banner
             + imports
-            + (f"{more}" f"Welcome to Oracle drgn-tools v{__version__}!\n")
-            + str(get_module_load_summary(prog))
-            + "\n"
+            + f"\nWelcome to Oracle drgn-tools v{__version__}!\n"
+            + db_info
         )
 
     def globals_func(globals_: Dict[str, Any]) -> Dict[str, Any]:
