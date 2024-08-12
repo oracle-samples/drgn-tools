caba4ef32879ec976906595802ff77bf62783e05
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-08-12 11:10:48 -0700

    tests: bt: get rid of the UEK4 exception
    There's no reason to exempt UEK4 from this test: it's not that drgn
    can't do stack traces on UEK4, it's really that "crashed_thread()"
    doesn't work on UEK4 vmcores. Let's work around that so we can still
    smoke test the actual bt operation.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- tests/test_bt.py
+++ tests/test_bt.py
@@ -1,17 +1,28 @@
-# Copyright (c) 2023, Oracle and/or its affiliates.
+# Copyright (c) 2024, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 import drgn
-import pytest
+from drgn.helpers.linux import cpu_curr
 
 from drgn_tools import bt
 
 
-@pytest.mark.skip_vmcore("*-uek4")
-def test_bt_smoke(prog, request, debuginfo_type):
+def test_bt_smoke(prog):
     if prog.flags & drgn.ProgramFlags.IS_LIVE:
         thread = prog.thread(1)
     else:
-        thread = prog.crashed_thread()
+        try:
+            thread = prog.crashed_thread()
+        except Exception:
+            # On x86_64 uek4, the sysrq does not actually trigger a panic, it
+            # triggers a NULL pointer dereference, which triggers an "oops", and
+            # that directly calls into the kexec code without ever calling
+            # panic(). Thus, panic_cpu == -1, and prog.crashing_cpu() page
+            # faults because it tries to index the wrong per-cpu variables.
+            # To handle this, use the x86_64-specific "crashing_cpu" variable.
+            # Note that on some drgn versions we get "FaultError", others we get
+            # "Exception". So we just catch Exception here.
+            pid = cpu_curr(prog, prog["crashing_cpu"]).pid.value_()
+            thread = prog.thread(pid)
 
     print("===== STACK TRACE [show_vars=False] =====")
     bt.bt(thread, show_vars=False)
