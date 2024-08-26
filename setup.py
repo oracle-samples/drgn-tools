3afc56146f54d09dfd1f6d3c1b7436eda7e638be
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-08-26 10:21:38 -0700

    Release v1.1.0
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 long_description = "drgn helper script repository"
 
-RELEASE_VERSION = "0.9.1"
+RELEASE_VERSION = "1.1.0"
 PACKAGES = ["drgn_tools"]
 if not os.environ.get("DRGN_TOOLS_V2_OMIT"):
     PACKAGES.append("drgn_tools.v2")
