cf8531199a1b76262176009fe4cc811af20fcee7
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-12-19 09:37:51 -0800

    heavyvm: install drgn 0.0.30
    Orabug: 37413891
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- testing/heavyvm/images.py
+++ testing/heavyvm/images.py
@@ -41,7 +41,7 @@ CONFIGURATIONS = [
         7,
         "x86_64",
         "https://yum.oracle.com/ISOS/OracleLinux/OL9/u5/x86_64/OracleLinux-R9-U5-x86_64-boot-uek.iso",  # noqa
-        ["drgn-0.0.29-1.0.1.el9.x86_64.rpm"],
+        ["drgn-0.0.30-1.0.1.el9.x86_64.rpm"],
     ),
     # OL8: UEK 6-7
     ImageInfo(
@@ -50,7 +50,7 @@ CONFIGURATIONS = [
         7,
         "x86_64",
         "https://yum.oracle.com/ISOS/OracleLinux/OL8/u10/x86_64/OracleLinux-R8-U10-x86_64-boot-uek.iso",  # noqa
-        ["drgn-0.0.29-1.0.1.el8.x86_64.rpm"],
+        ["drgn-0.0.30-1.0.1.el8.x86_64.rpm"],
     ),
     ImageInfo(
         8,
@@ -58,7 +58,7 @@ CONFIGURATIONS = [
         6,
         "x86_64",
         "https://yum.oracle.com/ISOS/OracleLinux/OL8/u10/x86_64/OracleLinux-R8-U10-x86_64-boot-uek.iso",  # noqa
-        ["drgn-0.0.29-1.0.1.el8.x86_64.rpm"],
+        ["drgn-0.0.30-1.0.1.el8.x86_64.rpm"],
     ),
     # OL7: UEK 4-6
     ImageInfo(
@@ -67,7 +67,7 @@ CONFIGURATIONS = [
         6,
         "x86_64",
         "https://yum.oracle.com/ISOS/OracleLinux/OL7/u9/x86_64/x86_64-boot-uek.iso",  # noqa
-        ["drgn-0.0.29-1.0.1.el7.x86_64.rpm"],
+        ["drgn-0.0.30-1.0.1.el7.x86_64.rpm"],
     ),
     ImageInfo(
         7,
@@ -75,7 +75,7 @@ CONFIGURATIONS = [
         5,
         "x86_64",
         "https://yum.oracle.com/ISOS/OracleLinux/OL7/u9/x86_64/x86_64-boot-uek.iso",  # noqa
-        ["drgn-0.0.29-1.0.1.el7.x86_64.rpm"],
+        ["drgn-0.0.30-1.0.1.el7.x86_64.rpm"],
     ),
     ImageInfo(
         7,
@@ -83,7 +83,7 @@ CONFIGURATIONS = [
         4,
         "x86_64",
         "https://yum.oracle.com/ISOS/OracleLinux/OL7/u9/x86_64/x86_64-boot-uek.iso",  # noqa
-        ["drgn-0.0.29-1.0.1.el7.x86_64.rpm"],
+        ["drgn-0.0.30-1.0.1.el7.x86_64.rpm"],
     ),
 ]
 NAME_TO_CONFIGURATION = {i.name: i for i in CONFIGURATIONS}
