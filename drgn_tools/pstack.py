133abc6d3624ebcb5df101288d42d79250240223
Stephen Brennan <stephen.s.brennan@oracle.com>
2025-08-14 11:24:57 -0700

    pstack: enable unwinding running tasks
    In most vmcores, userspace tasks are not actually running: the CPU is
    executing a kernel-mode interrupt handler that brings it to a halt.
    Thus, the userspace registers are stored to the kernel stack. But for
    hypervisor vmcores, the CPU may be executing user code directly. In that
    case, drgn's stack trace registers are in fact the userspace registers.
    Handle this (rare) case.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- drgn_tools/pstack.py
+++ drgn_tools/pstack.py
@@ -18,9 +18,9 @@ contains two approaches:
    works better in a kexec environment, because the root filesystem is not
    required, and userspace debuginfo is not consulted.
 
-This script is only tested on x86_64, and it's especially suited for Fedora and
-its derivatives, due to their inclusion of ".eh_frame" on runtime binaries, as
-well as ".gnu_debugdata" sections for address to symbol resolution.
+This script is tested on x86_64 and aarch64, and it's especially suited for
+Fedora and its derivatives, due to their inclusion of ".eh_frame" on runtime
+binaries, as well as ".gnu_debugdata" sections for address to symbol resolution.
 """
 import argparse
 import base64
@@ -41,10 +41,12 @@ from typing import List
 from typing import Tuple
 
 import drgn
+from drgn import Architecture
 from drgn import FaultError
 from drgn import Object
 from drgn import Program
 from drgn import sizeof
+from drgn import StackTrace
 from drgn.helpers.linux import access_remote_vm
 from drgn.helpers.linux import cpu_curr
 from drgn.helpers.linux import d_path
@@ -62,9 +64,18 @@ from drgn_tools.task import for_each_task_in_group
 log = logging.getLogger("drgn.pstack")
 
 
-def get_pt_regs(task: Object) -> Object:
+def task_saved_pt_regs(task: Object) -> Object:
     """
-    Create a ``struct pt_regs`` for the given task struct
+    Return the userspace registers for the given task struct
+
+    This returns the registers which were saved on entry to the kernel. For
+    vmcores generated via kexec and /proc/vmcore, all userspace tasks will have
+    registers stored on the stack, because every CPU should be interrupted and
+    halted. However, for vmcores which were created by a hypervisor, or for
+    live systems, userspace tasks may be directly executing, and any data stored
+    on the kernel stack is stale. Drgn does not provide an easy API to get this
+    info, but you can tell based on whether the stack pointer is a user or
+    kernel address.
 
     :param task: the ``struct task_struct *`` of this task
     :returns: a ``struct pt_regs`` value object
@@ -88,6 +99,64 @@ def get_pt_regs(task: Object) -> Object:
         - padding
     )
     return Object(prog, "struct pt_regs", address=regs_addr)
+
+
+def task_running_pt_regs(kstack: StackTrace) -> Object:
+    """
+    Create a ``struct pt_regs`` object from the top frame of a stack trace
+
+    This returns the registers for a task that is/was actively running. They
+    should be stored in the core dump metadata (e.g. PRSTATUS), and we can get
+    at them via drgn's stack trace object. Drgn's kernel Program won't be able
+    to unwind it anyway.
+
+    :param kstack: The kernel stack trace.
+    :returns: A ``struct pt_regs`` value object containing the user-space registers.
+    """
+    prog = kstack.prog
+    pt_regs = {}
+    tp = prog.type("struct pt_regs")
+    if prog.platform.arch == Architecture.X86_64:
+        rename = {
+            "rip": "ip",
+            "rbp": "bp",
+            "rax": "ax",
+            "rbx": "bx",
+            "rcx": "cx",
+            "rdx": "dx",
+            "rdi": "di",
+            "rsi": "si",
+            "rsp": "sp",
+            "rflags": "flags",
+        }
+        for name, value in kstack[0].registers().items():
+            if name in rename:
+                name = rename[name]
+            try:
+                tp.member(name)
+            except LookupError:
+                continue
+            pt_regs[name] = value
+    elif prog.platform.arch == Architecture.AARCH64:
+        pt_regs["regs"] = [0] * 31
+        pt_regs["pc"] = kstack[0].pc
+        for name, value in kstack[0].registers().items():
+            if name[0] == "x":
+                pt_regs["regs"][int(name[1:])] = value
+            elif name == "lr":  # an alias for x30
+                pt_regs["regs"][30] = value
+            else:
+                try:
+                    tp.member(name)
+                    pt_regs[name] = value
+                except LookupError:
+                    pass
+    else:
+        raise NotImplementedError(
+            f"Support for {prog.platform.arch} is not implemented"
+        )
+
+    return Object(prog, "struct pt_regs", value=pt_regs)
 
 
 def make_fake_pt_regs(up: Program, data: bytes) -> Object:
@@ -239,17 +308,26 @@ def dump(prog: Program) -> None:
             for thread in for_each_task_in_group(task, include_self=True):
                 tid = thread.pid.value_()
                 tcomm = thread.comm.string_().decode("utf-8", errors="replace")
-                regs = get_pt_regs(thread)
                 try:
-                    kstack = str(prog.stack_trace(thread))
+                    kstack = prog.stack_trace(thread)
                 except ValueError:
                     log.warning("skipped running TID %d ('%s')", tid, tcomm)
                     continue
+                if len(kstack) > 0 and (kstack[0].pc & (1 << 63)):
+                    # CPU was running in kernel mode, get the saved registers
+                    regs = task_saved_pt_regs(thread)
+                    kstack_str = str(kstack)
+                else:
+                    # CPU was in user mode. Drgn won't make be able to unwind
+                    # it, but we can take the top frame and get the original
+                    # registers for unwinding.
+                    regs = task_running_pt_regs(kstack)
+                    kstack_str = "<running in user mode>"
                 metadata["threads"].append(
                     {
                         "tid": tid,
                         "comm": tcomm,
-                        "kstack": kstack,
+                        "kstack": kstack_str,
                         "regs": base64.b64encode(regs.to_bytes_()).decode(),
                     },
                 )
@@ -334,7 +412,7 @@ def build_prog_from_dump(
 def print_user_stack_trace(regs: Object) -> None:
     """
     Prints the userspace stack trace for regs, with the module name included
-    for ecah frame. Including the module name is pretty important for userspace.
+    for each frame. Including the module name is pretty important for userspace.
     """
     prog = regs.prog_
     trace = prog.stack_trace(regs)
@@ -344,7 +422,7 @@ def print_user_stack_trace(regs: Object) -> None:
         try:
             mod = prog.module(frame.pc)
             off = frame.pc - mod.address_range[0]
-            mod_text = f" (in {mod.name} +{off:x})"
+            mod_text = f" (in {mod.name} +0x{off:x})"
         except LookupError:
             pass
         print("    " + line.rstrip() + mod_text)
@@ -449,7 +527,7 @@ def pstack_print_process(task: Object) -> None:
     print(f"[PID: {task.pid.value_()} COMM: {comm}]")
     prog = task.prog_
     if not task.mm:
-        print(str(prog.stack_trace(task)).replace("\n", "\n  "))
+        print("  " + str(prog.stack_trace(task)).replace("\n", "\n  "))
         return
 
     user_prog = build_prog_from_mm(task.mm)
@@ -459,8 +537,17 @@ def pstack_print_process(task: Object) -> None:
         tid = thread.pid.value_()
         tcomm = thread.comm.string_().decode("utf-8", errors="replace")
         print(f"  Thread {i} TID={tid} ('{tcomm}')")
-        print("    " + str(prog.stack_trace(thread)).replace("\n", "\n    "))
-        regs = get_pt_regs(task)
+        kstack = prog.stack_trace(thread)
+        if len(kstack) > 0 and (kstack[0].pc & (1 << 63)):
+            # Kernel stack is indeed a kernel stack, print it
+            print(
+                "    " + str(prog.stack_trace(thread)).replace("\n", "\n    ")
+            )
+            regs = task_saved_pt_regs(task)
+        else:
+            # CPU was in user-mode, print that instead:
+            print("    <running in user mode>")
+            regs = task_running_pt_regs(kstack)
         fake_regs = make_fake_pt_regs(user_prog, regs.to_bytes_())
         print_user_stack_trace(fake_regs)
 
