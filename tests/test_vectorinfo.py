2b309e4e126c77723a1c08c8181f485f579c2a35
Stephen Brennan <stephen.s.brennan@oracle.com>
2025-10-31 15:04:30 -0700

    tests: vectorinfo: skip for UEK4-5, and aarch64
    The functions themselves raise appropriate errors, but we don't want the
    tests to fail on these vmcores.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- tests/test_vectorinfo.py
+++ tests/test_vectorinfo.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+import pytest
+
 from drgn_tools import vectorinfo
 
 
-def test_vectorinfo(prog):
+def test_vectorinfo(prog, kver):
+    if kver.arch != "x86_64":
+        pytest.skip("Only x86_64 is supported")
+    if kver.uek_version is not None and kver.uek_version < 6:
+        pytest.skip("UEK6 or later is required")
     vectorinfo.print_vector_matrix(prog)
     vectorinfo.print_vectors(prog, True)
