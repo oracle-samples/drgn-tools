3536fa0a85bfcee5dfee99865cfd026fb4b4c22f
Stephen Brennan <stephen.s.brennan@oracle.com>
2026-02-12 16:07:16 -0800

    tests: add test to exercise slabdump
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- tests/test_slabinfo.py
+++ tests/test_slabinfo.py
@@ -1,7 +1,14 @@
 # Copyright (c) 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+from drgn.helpers.linux.slab import find_slab_cache
+
 from drgn_tools import slabinfo
 
 
-def test_blockinfo(prog):
+def test_slabinfo(prog):
     slabinfo.print_slab_info(prog)
+
+
+def test_slabdump(prog):
+    cache = find_slab_cache(prog, "kmalloc-256")
+    slabinfo.dump_slab_objects(cache, limit=10)
