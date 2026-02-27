967a51d6cf0517b0666f8b446e5b3a3c745db56f
Junxiao Bi <junxiao.bi@oracle.com>
2026-02-27 04:00:08 +0000

    blockinfo: add disk driver
    For scsi/nvme, it will dump the physical driver module name.
    For md, it will dump the raid driver.
    For dm, it will dump target driver.
    
    Signed-off-by: Junxiao Bi <junxiao.bi@oracle.com>

--- drgn_tools/blockinfo.py
+++ drgn_tools/blockinfo.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2026, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 import argparse
+import re
 
 import drgn
+from drgn import Object
 from drgn.helpers.linux.block import for_each_disk
 
 from drgn_tools.block import get_inflight_io_nr
@@ -10,8 +12,43 @@ from drgn_tools.block import get_max_io_inflight_ns
 from drgn_tools.block import queue_freezed_depth
 from drgn_tools.block import queue_usage_counter
 from drgn_tools.corelens import CorelensModule
+from drgn_tools.dm import dm_target_driver
+from drgn_tools.md import md_raid_driver
+from drgn_tools.nvme import nvme_disk_driver
+from drgn_tools.scsi import scsi_disk_driver
 from drgn_tools.table import print_table
 from drgn_tools.util import timestamp_str
+
+
+def get_disk_type(disk: Object) -> str:
+    diskname = disk.disk_name.string_().decode()
+    patterns = {
+        "nvme": r"^nvme\d+(n\d+)?$",
+        "scsi": r"^sd[a-z]+$",
+        "md": r"^md\d+$",
+        "dm": r"^dm-\d+$",
+        "loop": r"^loop\d+$",
+        "pmem": r"^pmem\d+$",
+    }
+    for dtype, pattern in patterns.items():
+        if re.match(pattern, diskname):
+            return dtype
+
+    return "unknown"
+
+
+def get_disk_driver(prog: drgn.Program, disk: Object) -> str:
+    dtype = get_disk_type(disk)
+    if dtype == "nvme":
+        return nvme_disk_driver(prog, disk)
+    elif dtype == "scsi":
+        return scsi_disk_driver(prog, disk)
+    elif dtype == "md":
+        return md_raid_driver(prog, disk)
+    elif dtype == "dm":
+        return dm_target_driver(prog, disk)
+    else:
+        return dtype
 
 
 def print_block_devs_info(prog: drgn.Program) -> None:
@@ -23,6 +60,7 @@ def print_block_devs_info(prog: drgn.Program) -> None:
             "MAJOR",
             "GENDISK",
             "NAME",
+            "DRIVER",
             "REQUEST_QUEUE",
             "Inflight I/Os",
             "Max Inflight time",
@@ -42,6 +80,7 @@ def print_block_devs_info(prog: drgn.Program) -> None:
                 str(major),
                 gendisk,
                 name,
+                get_disk_driver(prog, disk),
                 rq,
                 str(ios),
                 timestamp_str(get_max_io_inflight_ns(prog, disk)),
