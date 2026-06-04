5e049de86c830c859c3fc157ee22cacd3ded1e46
Stephen Brennan <stephen.s.brennan@oracle.com>
2026-06-04 08:02:02 -0700

    cmdline: simplify and add assertion test
    There's no need for a separate "show_cmdline()" function.
    Also, /proc/cmdline can be used to trivially verify the result. Do so.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,10 +1,16 @@
-# Copyright (c) 2023, Oracle and/or its affiliates.
+# Copyright (c) 2023, 2026, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 from drgn_tools import cmdline
 from tests import DrgnToolsTestCase
+from tests import skip_unless_live
 
 
 class TestCmdline(DrgnToolsTestCase):
+    @skip_unless_live
     def test_cmdline(self):
+        with open("/proc/cmdline", "r") as f:
+            found = f.read().rstrip("\n")
+        self.assertEqual(cmdline.get_cmdline(self.prog), found)
+
+    def test_cmdline_smoke(self):
         cmdline.get_cmdline(self.prog)
-        cmdline.show_cmdline(self.prog)
