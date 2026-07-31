2abe803d6fd8a7d89e7995f49555cbc8f51702db
Aryamann Sheoran <aryamann.sheoran@oracle.com>
2026-07-31 08:43:22 +0000

    mlx5: remove low-value smoke tests
    Remove the device discovery smoke test, static module metadata assertions,
    and the test which expects malformed descriptors to be silently accepted.
    Keep the suite focused on concrete collection, decoding, selection, and
    rendering behavior.
    
    Orabug: 39751588
    Signed-off-by: Aryamann Sheoran <aryamann.sheoran@oracle.com>

--- tests/test_mlx5.py
+++ tests/test_mlx5.py
@@ -4,22 +4,13 @@
 import argparse
 from operator import itemgetter
 
-from drgn import cast
-from drgn import container_of
-from drgn.helpers.linux.net import netdev_priv
-
 from drgn_tools import mlx5
-from drgn_tools.corelens import all_corelens_modules
 from drgn_tools.mlx5_support import selection
 from drgn_tools.mlx5_support.collect_device import DeviceRecord
-from drgn_tools.mlx5_support.collect_device import for_each_mlx5_core_dev
-from drgn_tools.mlx5_support.collect_device import mlx5_core_ib_device
-from drgn_tools.mlx5_support.collect_device import mlx5_netdev
 from drgn_tools.mlx5_support.render import _render_cqs
 from drgn_tools.mlx5_support.render import _render_dumps
 from drgn_tools.mlx5_support.render import _render_qps
 from drgn_tools.mlx5_support.render import render_report
-from tests import DrgnToolsTestCase
 from tests.unittest_helpers import load_test_functions
 from tests.unittest_helpers import parametrize
 from tests.unittest_helpers import raises
@@ -127,61 +118,6 @@ class _FakeProgram:
 _FAKE_PROG = _FakeProgram()
 
 
-class TestMlx5DeviceDiscovery(DrgnToolsTestCase):
-    def test_driver_device_relationships(self):
-        try:
-            self.prog["mlx5_core_driver"]
-        except LookupError:
-            self.skipTest("mlx5_core is not loaded")
-
-        mdevs = list(for_each_mlx5_core_dev(self.prog))
-        if not mdevs:
-            self.skipTest("no devices are bound to mlx5_core")
-
-        sf_type = int(self.prog.constant("MLX5_COREDEV_SF"))
-        for mdev in mdevs:
-            self.assertTrue(mdev)
-            if int(mdev.coredev_type) == sf_type:
-                adev = container_of(
-                    cast("struct device *", mdev.device),
-                    "struct auxiliary_device",
-                    "dev",
-                )
-                sf_dev = container_of(adev, "struct mlx5_sf_dev", "adev")
-                self.assertEqual(int(sf_dev.mdev), int(mdev))
-            else:
-                self.assertEqual(int(mdev.pdev.dev.driver_data), int(mdev))
-
-            netdev = mlx5_netdev(mdev)
-            if netdev:
-                priv = netdev_priv(netdev, "struct mlx5e_priv")
-                self.assertEqual(int(priv.mdev), int(mdev))
-                channels = priv.channels
-                for index in range(int(channels.num)):
-                    channel = channels.c[index]
-                    if not channel:
-                        continue
-                    self.assertEqual(int(channel.priv), int(priv))
-                    self.assertEqual(int(channel.mdev), int(mdev))
-                    self.assertEqual(int(channel.netdev), int(netdev))
-                    self.assertGreaterEqual(int(channel.rq.rqn), 0)
-                    self.assertGreaterEqual(int(channel.rq.cq.mcq.cqn), 0)
-                    for tc in range(int(channel.num_tc)):
-                        self.assertGreaterEqual(int(channel.sq[tc].sqn), 0)
-                        self.assertGreaterEqual(
-                            int(channel.sq[tc].cq.mcq.cqn), 0
-                        )
-                    self.assertGreaterEqual(int(channel.icosq.sqn), 0)
-                    self.assertGreaterEqual(
-                        int(channel.icosq.cq.mcq.cqn), 0
-                    )
-
-            ib_device = mlx5_core_ib_device(mdev)
-            if ib_device:
-                ibdev = container_of(ib_device, "struct mlx5_ib_dev", "ib_dev")
-                self.assertEqual(int(ibdev.mdev), int(mdev))
-
-
 def _args(**overrides):
     values = dict.fromkeys(
         "dev netdev ip cqn eqn qpn sqn rqn maxqueues maxcq maxeq maxqp".split()
@@ -237,24 +173,6 @@ def _addressed_mapping_collector(monkeypatch):
 
 def _fake_qp(address, hw_qpn):
     return {"_address": address, "ibqp": {"qp_num": 1}, "qpn": hw_qpn}
-
-
-def test_module_contract():
-    module = all_corelens_modules()["mlx5"]
-
-    assert (
-        module.name,
-        module.run_when,
-        module.need_dwarf,
-        module.live_ok,
-    ) == (
-        "mlx5",
-        "never",
-        False,
-        True,
-    )
-    assert module.skip_unless_have_kmods == ["mlx5_core"]
-    assert module.debuginfo_kmods == ["mlx5_core", "mlx5_ib"]
 
 
 def test_qp_registry_keeps_distinct_objects_with_the_same_logical_qpn(
@@ -854,13 +772,6 @@ def test_decode_uses_new_program_enum_values_without_a_code_table():
         mlx5._decode_wqe(_FAKE_PROG, bytes(send))["opcode_display"]
         == "TEST_NEW(0x7f)"
     )
-
-
-def test_short_descriptors_return_unknown_fields_without_index_errors():
-    assert mlx5._decode_cqe(_FAKE_PROG, b"")["opcode_value"] is None
-    assert mlx5._decode_eqe(_FAKE_PROG, b"")["type_value"] is None
-    assert mlx5._decode_wqe(_FAKE_PROG, b"")["qpn"] is None
-    assert mlx5._decode_rq_wqe(b"")["dma_addr"] is None
 
 
 def test_render_report_preserves_compact_summary_contract(capsys):
