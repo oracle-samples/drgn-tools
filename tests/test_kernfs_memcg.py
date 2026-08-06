0e5cb4b6a5f141c5fb680ad7711cd3dc8091f979
Stephen Brennan <stephen.s.brennan@oracle.com>
2026-08-06 16:58:26 -0700

    tests: kernfs_memcg: speed up test
    We iterate over every slab object in the kernfs_node slab (which could
    be merged), just to find some memcg-related ones. Instead, let's grab
    known ones and test them directly.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- tests/test_kernfs_memcg.py
+++ tests/test_kernfs_memcg.py
@@ -14,13 +14,35 @@ class TestKernfsMemcg(DrgnToolsTestCase):
         kernfs_memcg.dump_memcgroup_hierarchy(self.prog)
 
     def test_kernfs_node_of_memcgroup(self):
-        count = 0
-        for kn in kernfs_memcg.for_each_kernfs_node(self.prog):
-            if kernfs_memcg.kernfs_node_of_memcgroup(kn):
-                count = count + 1
-            if count >= 5:
-                print("Found 5 memcgroup, kernfs_node objects.")
-                break
+        cgroup_subsys = self.prog["cgroup_subsys"]
+
+        # Test against the memcg root kn:
+        memory_cgrp_id = self.prog.constant("memory_cgrp_id")
+        memory_subsys = cgroup_subsys[memory_cgrp_id]
+        memcg_kn = memory_subsys.root.cgrp.kn
+        self.assertTrue(kernfs_memcg.kernfs_node_of_cgroup(memcg_kn))
+        self.assertTrue(kernfs_memcg.kernfs_node_of_memcgroup(memcg_kn))
+
+        # Test against a kn associated with another cgroup. There may not be a
+        # non-memory cgroup on the system, for example on cgroup v1. So it's ok
+        # that we only assert this if we manage to find one.
+        for i in range(self.prog.constant("CGROUP_SUBSYS_COUNT")):
+            subsys = cgroup_subsys[i]
+            if subsys and subsys.root != memory_subsys.root:
+                other_cg_kn = subsys.root.cgrp.kn
+                self.assertTrue(
+                    kernfs_memcg.kernfs_node_of_cgroup(other_cg_kn)
+                )
+                self.assertFalse(
+                    kernfs_memcg.kernfs_node_of_memcgroup(other_cg_kn)
+                )
+
+        # Finally, there's plenty of other kernfs nodes on the system, e.g. the
+        # one associated with the sysfs root. Use that as an example which is
+        # neither cgroup, nor memcg.
+        other_kn = self.prog["sysfs_root"].kn
+        self.assertFalse(kernfs_memcg.kernfs_node_of_cgroup(other_kn))
+        self.assertFalse(kernfs_memcg.kernfs_node_of_memcgroup(other_kn))
 
     def test_get_num_active_mem_cgroups(self):
         count = kernfs_memcg.get_num_active_mem_cgroups(self.prog)
